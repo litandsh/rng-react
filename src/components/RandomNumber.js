cd000a0ee4ad4379dd4eb518524b90c17f0d8a0c
Andrii Litviniuk <litandsh@gmail.com>
2022-10-04 18:53:15 +0200

    feat: create new component

---
+++ src/components/RandomNumber.js
@@ -0,0 +1,10 @@
+function RandomNumber() {
+    return (
+        <div>
+            <h1>100</h1>
+            <button>Generate new random number</button>
+        </div>
+    )
+}
+
+export default RandomNumber
