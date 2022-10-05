02f2787155fd138bfb789e5442fbf4e5e95cb530
Andrii Litviniuk <litandsh@gmail.com>
2022-10-05 10:02:26 +0200

    feat: implement useState hook

--- src/components/RandomNumber.js
+++ src/components/RandomNumber.js
@@ -1,8 +1,17 @@
+import { useState } from "react"
+import generateRandomNum from "../utils/generateRandomNum";
+
+
 function RandomNumber() {
+    const [randomNum, setRandomNum] = useState(generateRandomNum());
+    const changeRandomNum = () => {
+        setRandomNum(generateRandomNum());
+    }
+
     return (
         <div>
-            <h1>100</h1>
-            <button>Generate new random number</button>
+            <h1>{randomNum}</h1>
+            <button onClick={ changeRandomNum }>Generate new random number</button>
         </div>
     )
 }
---
+++ src/utils/generateRandomNum.js
@@ -0,0 +1,5 @@
+function generateRandomNum() {
+    return Math.floor(Math.random() * 100)
+}
+
+export default generateRandomNum
